774636dac7c452c0555eb7574aefe31126f38d0c
ErnTrgt <erntrgt10@gmail.com>
2024-05-22 17:48:01 +0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("deneme");
+const typingText = document.querySelector(".typing-text p");
+inpField = document.querySelector(".wrapper .input-field");
+
+let charIndex = 0;
+
+function randomParagraph() {
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    paragraphs[randIndex].split("").forEach((span) => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+
+    if (characters[charIndex].innerText === typedChar) {
+        console.log("correct");
+    } else {
+        console.log("false");
+    }
+    charIndex++;
+}
+
+randomParagraph();
+inpField.addEventListener("input", initTyping);
